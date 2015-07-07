1667e89e692efc76f85742062e29e11e81b85421
Ryan Davis <rsdavis@nat-oitwireless-inside-vapornet100-a-14111.Princeton.EDU>
2015-07-07 12:21:03 -0400

    Added units tests and error checking

--- H5Grid.hpp
+++ H5Grid.hpp
@@ -6,17 +6,25 @@
 //! A simple interface for storing/accessing grid data in hdf5 files
 
 class H5Grid {
+
     private:
         hid_t file_id;
+        int Nx, Ny, Nz;
         void create_group(const char * dataset_name);
+
     public:
-        int open(const char * filename, const char * mode, int * dims, int &ndims);
+        H5Grid();
+        int open(std::string filename, std::string mode, int &nx, int &ny, int &nz);
         int read_dataset(const char * dataset_name, double * dataset);
         int write_dataset(const char * dataset_name, double * dataset);
         int set_attribute(std::string attr_name, int attr_value);
         int get_attribute(std::string attr_name, int &attr_value);
-        void close();
+        int close();
 };
+
+H5Grid :: H5Grid () {
+    file_id = 0;
+}
 
 void H5Grid :: create_group(const char * dataset_name)
 {
@@ -42,58 +50,63 @@ void H5Grid :: create_group(const char * dataset_name)
 }
 
 
-int H5Grid :: open(const char * filename, const char * mode, int * dims, int &ndims)
+int H5Grid :: open(std::string filename, std::string mode, int &nx, int &ny, int &nz)
 {
     /**
-    * @param filename Name of the hdf5 file
-    * @param mode "r", "w", or "a"
-    * @param dims an array of extents of the grid to be stored
-    * @param ndims the number of dimensions of the grid
+    @param filename Name of the hdf5 file
+    @param mode of file access "r", "w", or "a"
+    @param nx number of grid points in x dimension
+    @param ny number of grid points in y dimension
+    @param nz number of grid points in z dimension
+    **/
+
+    /**
+    nx, ny, nz must be variables, and cannot be constant integers (e.g. "nx" instead of "100")
+    For 2D data, nz should be set to 0.
     **/
 
 
     unsigned read, write, append;
 
-    read = (mode[0] == 'r');
-    write = (mode[0] == 'w');
-    append = (mode[0] == 'a');
+    read = (mode == "r");
+    write = (mode == "w");
+    append = (mode == "a");
 
     // check that one is true
     if ( (read || write || append) != 1) return 1;
 
+    // check that a file is not already open
+    if (file_id != 0) return 2;
+
 
     if ( write ) {
-        file_id = H5Fcreate(filename, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
-        
-        // write the dims to an file attribute
-        hsize_t attr_dim = ndims;
-        hid_t space_id = H5Screate_simple(1, &attr_dim, NULL);
-        hid_t attr_id = H5Acreate(file_id, "DIMS", H5T_NATIVE_INT, space_id, H5P_DEFAULT, H5P_DEFAULT);
-        H5Awrite(attr_id, H5T_NATIVE_INT, dims);
-        H5Sclose(space_id);
-        H5Aclose(attr_id);
+        file_id = H5Fcreate(filename.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+
+        set_attribute("/Nx", nx);
+        set_attribute("/Ny", ny);
+        set_attribute("/Nz", nz);
 
     } else if (read || append) {
-        FILE * file_exists = fopen(filename, "r");
-        if (file_exists == NULL) return 2;
+        FILE * file_exists = fopen(filename.c_str(), "r");
+        if (file_exists == NULL) return 3;
         else fclose(file_exists);
 
-        if (H5Fis_hdf5(filename) < 1) return 3;
+        if (H5Fis_hdf5(filename.c_str()) < 1) return 4;
 
         if (read)
-            file_id = H5Fopen(filename, H5F_ACC_RDONLY, H5P_DEFAULT);
+            file_id = H5Fopen(filename.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
         if (append)
-            file_id = H5Fopen(filename, H5F_ACC_RDWR, H5P_DEFAULT);
+            file_id = H5Fopen(filename.c_str(), H5F_ACC_RDWR, H5P_DEFAULT);
 
-        hsize_t attr_dim[1];
-        hid_t attr_id = H5Aopen(file_id, "DIMS", H5P_DEFAULT);
-        H5Aread(attr_id, H5T_NATIVE_INT, dims);
-        hid_t space_id = H5Aget_space(attr_id);
-        int stat = H5Sget_simple_extent_dims(space_id, attr_dim, NULL);
-        ndims = (int) (attr_dim[0]);
-        H5Sclose(space_id);
-        H5Aclose(attr_id);
+        get_attribute("/Nx", nx);
+        get_attribute("/Ny", ny);
+        get_attribute("/Nz", nz);
+
     }
+
+    Nx = nx;
+    Ny = ny;
+    Nz = nz;
 
     return 0;
 }
@@ -137,9 +150,18 @@ int H5Grid :: set_attribute(std::string attr_name, int  attr_value)
     * @param attr_value Value of the attribute
     **/
 
+    /**
+    The attr_name must have atleast one backslash.
+    For example, setting an attribute named my_attr in the root group
+    should be specified as "/my_attr"
+    */
+
     std::string path;
     std::string name;
     parse(attr_name, path, name);
+
+    if (path.size() == 0) return 1;
+    if (name.size() == 0) return 2;
 
     create_group(attr_name.c_str());
     hid_t space_id = H5Screate(H5S_SCALAR);
@@ -171,7 +193,6 @@ int H5Grid :: get_attribute(std::string attr_name, int &attr_value)
     std::string name;
 
     parse(attr_name, path, name);
-    std::cout << path << ":" << name << std::endl;
 
     if (H5Aexists_by_name(file_id, path.c_str(), name.c_str(), H5P_DEFAULT) <= 0) return 1;
 
@@ -183,22 +204,15 @@ int H5Grid :: get_attribute(std::string attr_name, int &attr_value)
     return 0;
 }
 
-void H5Grid :: close()
+int H5Grid :: close()
 {
-}
+    if (file_id == 0) {
+        // no file opened
+        return 1;
+    } else {
+        H5Fclose(file_id);
+        file_id = 0;
+    }
 
-/*
-int main(int argc, char ** argv)
-{
-
-    int dims[3];
-    dims[0] = 100;
-    dims[1] = 100;
-    dims[2] = 100;
-
-
-    H5Grid out;
-    out.open("out.h5", "w", dims, 3);
     return 0;
 }
-*/
--- H5Grid_test.cpp
+++ H5Grid_test.cpp
@@ -2,50 +2,74 @@
 #include "H5Grid.hpp"
 #include "gtest/gtest.h"
 
+TEST(H5GridTest, OpenClose)
+{
+    H5Grid h0, h1;
+    int nx = 10;
+    int ny = 20;
+    int nz = 0;
+    int stat;
 
-/*
-TEST(Open, WriteMode) {
+    // check that it opens
+    stat = h0.open("test2.h5", "w", nx, ny, nz);
+    ASSERT_EQ(stat, 0);
+
+    // check that it cant open anything else
+    stat = h0.open("test3.h5", "w", nx, ny, nz);
+    EXPECT_NE(stat, 0);
+
+    stat = h0.close();
+    EXPECT_EQ(stat, 0);
+
+    // check for already closed error
+    stat = h0.close();
+    EXPECT_NE(stat, 0);
+
+    nx = 1;
+    ny = 1;
+    nz = 1;
+
+    // check open in read mode
+    stat = h1.open("test2.h5", "r", nx, ny, nz);
+    ASSERT_EQ(stat, 0);
+    EXPECT_EQ(nx, 10);
+    EXPECT_EQ(ny, 20);
+    EXPECT_EQ(nz, 0);
+    h1.close();
+}
+
+
+TEST(H5GridTest, SetGetAttr)
+{
     H5Grid h5;
-    int ndims = 3;
-    int dims[ndims];
-    dims[0] = 100;
-    dims[1] = 120;
-    dims[2] = 140;
-    h5.open("test.h5", "w", dims, ndims);
+    int nx = 10;
+    int ny = 20;
+    int nz = 0;
+    h5.open("test.h5", "w", nx, ny, nz);
+
+    int temp = 0;
+    int stat;
+    stat = h5.set_attribute("Temp", 50);
+    EXPECT_NE(stat, 0);
+
+    stat = h5.set_attribute("/Temp", 50);
+    ASSERT_EQ(stat, 0);
+
+    stat = h5.get_attribute("/Temp", temp);
+    ASSERT_EQ(stat, 0);
+    EXPECT_EQ(temp, 50);
+
+    stat = h5.set_attribute("/path/to/my/crazy/attr", 100);
+    ASSERT_EQ(stat, 0);
+    int attr;
+    stat = h5.get_attribute("/path/to/my/crazy/attr", attr);
+    EXPECT_EQ(attr, 100);
+    
     h5.close();
 }
-
-TEST(Open, ReadMode) {
-    int dims[3];
-    int ndims;
-    H5Grid h5;
-    h5.open("test.h5", "r", dims, ndims);
-    EXPECT_EQ(ndims, 3);
-    h5.close();
-}
-
-TEST(WriteTest, Writes) {
-    H5Grid h5;
-    h5.open("test.h5", "w", dims, 3);
-    write_dataset(dataset_name, dataset);
-}
-*/
 
 
 int main(int argc, char ** argv) {
-    /*
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-    */
-    int dims[3] = {100, 120, 140};
-    int ndims = 3;
-    int value;
-    H5Grid h5;
-    h5.open("test.h5", "w", dims, ndims);
-    h5.set_attribute("/path/to/my/Nx", 100);
-    int stat = h5.get_attribute("/path/to/my/Nx", value);
-    printf("stat = %d\n", stat);
-    h5.close();
-    std::cout << value << std::endl;
-    return 0;
 }
